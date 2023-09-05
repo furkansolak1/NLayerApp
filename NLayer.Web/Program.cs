bd251ff4f534b905b07322a7a3df4c10c5a8d69f
furkansolak1 <107087029+furkansolak1@users.noreply.github.com>
2023-09-05 19:01:53 +0300

    update 1

--- NLayer.Web/Program.cs
+++ NLayer.Web/Program.cs
@@ -1,8 +1,10 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using NLayer.Repository;
 using NLayer.Service.Mapping;
+using NLayer.Service.Validations;
 using NLayer.Web.Modules;
 using System.Reflection;
 
@@ -15,7 +17,7 @@ namespace NLayer.Web
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
             builder.Services.AddAutoMapper(typeof(MapProfile));
 
             builder.Services.AddDbContext<AppDbContext>(x =>
