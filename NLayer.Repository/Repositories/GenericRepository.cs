eda5f899dce4fdf7b034c04579e768367c38397b
furkansolak1 <107087029+furkansolak1@users.noreply.github.com>
2023-09-12 01:19:18 +0300

    dotnet7

--- NLayer.Repository/Repositories/GenericRepository.cs
+++ NLayer.Repository/Repositories/GenericRepository.cs
@@ -14,6 +14,7 @@ namespace NLayer.Repository.Repositories
         protected readonly AppDbContext _context;
         private readonly DbSet<T> _dbSet;
 
+
         public GenericRepository(AppDbContext context)
         {
             _context = context;
